e1b391f73d2a319092f18e113c59043cfaaae5c7
CrimsonSpark <zandrewepener012@gmail.com>
2024-03-19 21:32:19 +0200

    Implement semi-dynamic model
    This allows for the dynamic importing of page bodies. It also makes it so scripts and stylesheets are only loaded once per page reload, making the website's performance improve significantly. This approach also removes that unpleasant popping effect on the navbar every time a new menu item is selected.

---
+++ src/scripts/main.js
@@ -0,0 +1,38 @@
+const zip = (...arrays) => arrays[0].map((_, c) => arrays.map((row) => row[c]));
+
+const Pages = [
+  "home.html",
+  "timetable.html",
+  "discussion.html",
+  "share-resources.html",
+  "about-us.html",
+  "contact-us.html",
+];
+
+loadContent = (Page) => {
+  const PagePath = `pages/${Page}`;
+  $.get(PagePath, (content) => {
+    if (content) {
+      $(".content").html(content);
+    }
+  });
+};
+
+var currOpt;
+$(document).ready(() => {
+  const Spans = document.querySelectorAll(".navbar menu li span");
+  for (let i = 0; i < Pages.length; i++) {
+    const Span = Spans[i];
+    const Page = Pages[i];
+    Span.addEventListener("click", () => {
+      loadContent(Page);
+      document.getElementById("check").checked = false;
+      currOpt.parentNode.classList.remove("active");
+      Span.parentNode.classList.add("active");
+      currOpt = Span;
+    });
+  }
+  currOpt = Spans[0];
+  currOpt.parentNode.classList.add("active");
+  loadContent(Pages[0]);
+});
