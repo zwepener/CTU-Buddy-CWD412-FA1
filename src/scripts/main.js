b07c50ef506924af698bd59ae081f9d8700e9535
CrimsonSpark <zandrewepener012@gmail.com>
2024-03-29 02:37:16 +0200

    Update Main HTML File
    Update content injection function with efficiency in mind.
    Cleanup Code.
    Misc. Fixes.
    Apply Formatting.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
-const Pages = [
-  "home.html",
-  "timetable.html",
-  "discussion.html",
-  "share-resources.html",
-  "about-us.html",
-  "contact-us.html",
+const NavMenuItemIDs = [
+  "home",
+  "timetable",
+  "discussion",
+  "share-resources",
+  "about-us",
+  "contact-us",
 ];
 
 function loadContent(Page) {
@@ -17,24 +17,27 @@ function loadContent(Page) {
 }
 
 var currOpt;
-$(document).ready(() => {
-  const MenuItems = document.querySelectorAll(".navbar menu li");
-  for (let i = 0; i < Pages.length; i++) {
-    const MenuItem = MenuItems[i];
-    const Page = Pages[i];
+function configureNavbar() {
+  NavMenuItemIDs.forEach((NavMenuItemID) => {
+    const MenuItem = document.getElementById(NavMenuItemID).parentElement;
     MenuItem.addEventListener("click", () => {
-      loadContent(Page);
+      loadContent(`${NavMenuItemID}.html`);
       document.getElementById("check").checked = false;
       currOpt.classList.remove("active");
       MenuItem.classList.add("active");
       currOpt = MenuItem;
     });
-  }
-  currOpt = MenuItems[0];
+  });
+  currOpt = document.getElementById(NavMenuItemIDs[0]).parentElement;
   currOpt.classList.add("active");
-  loadContent(Pages[0]);
+  loadContent(`${NavMenuItemIDs[0]}.html`);
+}
+
+$(document).ready(() => {
+  configureNavbar();
 });
 
+// Default Functions
 function defaultAlert() {
   alert(
     "Due to the static nature of FA 1, no logic has been added to this element yet."
