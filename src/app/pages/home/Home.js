7698eb2f61b4f0d63b3a700bb82d84cf0cabd354
CrimsonSpark <zandrewepener012@gmail.com>
2024-04-29 22:17:23 +0200

    Update Home Page
    • Fixed any asset reference errors caused by the folder structure change
    • Deleted unused assets
    • Added changes from Phase 2

--- src/app/pages/home/Home.js
+++ src/app/pages/home/Home.js
@@ -1,19 +1,34 @@
-import welcome_img from '../../resources/images/welcome.jpg';
-import decor_1_img from '../../resources/images/home-decor-1.jpg';
-import decor_2_img from '../../resources/images/home-decor-2.png';
+import decor_1_img from "./assets/static/images/home-decor-1.jpg";
+import decor_2_img from "./assets/static/images/home-decor-2.png";
+import decor_3_img from "./assets/static/images/home-decor-3.png";
 
-import './Home.css';
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="default-box content" id="home-container">
-      <section id="welcome">
-        <div className="image-container">
-          <img src={welcome_img} alt="Welcome" />
-          <img src={decor_1_img} alt="decor-1" />
-          <img src={decor_2_img} alt="decor-2" />
+    <div id="home-container">
+      <section class="hero">
+        <h1>Welcome to CTU Buddy</h1>
+        <p>Explore our world-class educational features.</p>
+      </section>
+
+      <section class="s default-box">
+        <h2 class="header">
+          CTU
+          <br />
+          Buddy
+        </h2>
+        <div class="wave">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </section>
+      <div class="default-box content image-container">
+        <img class="default-box" src={decor_1_img} alt="Home Decor 1" />
+        <img class="default-box" src={decor_2_img} alt="Home Decor 2" />
+        <img class="default-box" src={decor_3_img} alt="Home Decor 3" />
+      </div>
     </div>
   );
 }
